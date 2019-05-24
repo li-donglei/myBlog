add8f26d718b0b3ad1c9f950bb451258504759c6
li-donglei <1072677403@qq.com>
2019-05-24 20:35:26 +0800

    update

--- src/main/java/com/donglei/myblog/controller/BackstageController.java
+++ src/main/java/com/donglei/myblog/controller/BackstageController.java
@@ -1,5 +1,9 @@
 package com.donglei.myblog.controller;
 
+import com.donglei.myblog.entity.Blog;
+import com.donglei.myblog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,9 +11,15 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class BackstageController {
+
+    @Autowired
+    BlogService blogService;
+
 
     @RequestMapping("/admin")
     public ModelAndView backstage(ModelAndView modelAndView){
@@ -41,6 +51,15 @@ public class BackstageController {
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("redirect:/admin");
+        return modelAndView;
+    }
+
+    @RequestMapping("/adminBlog")
+    public ModelAndView adminBlog(ModelAndView modelAndView){
+        List<Blog> blog= blogService.selectBlog();
+        Collections.reverse(blog);//blog逆序
+        modelAndView.addObject("blog",blog);
+        modelAndView.setViewName("backstage/backstage");
         return modelAndView;
 
     }
